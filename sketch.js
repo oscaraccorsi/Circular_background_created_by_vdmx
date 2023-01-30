e3e39499bd9f246f9383ef8942eab2a14c649fa6
oscaraccorsi <91617877+oscaraccorsi@users.noreply.github.com>
2023-01-30 18:04:43 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 //let baseURLSound = 'https://oscaraccorsi.github.io/mp3_files/';
 
 //const vol = new Tone.Volume(0).toDestination();
@@ -6,21 +7,36 @@
 
 let baseURLBack = 'https://oscaraccorsi.github.io/backgrounds/';
 let baseURLImage = 'https://oscaraccorsi.github.io/pictures/';
+let baseUrlPictures = ' https://oscaraccorsi.github.io/tartan/';
 let logo;
 let xLogo;
 let img;
-let pictureList = ['01.png', '02.png', 
-                   '03.png', '04.png',
-                   '05.png', '06.png', 
-                   '07.png', '08.png', 
-                   '09.png', '10.png', 
-                   '11.png', '12.png',
-                   '13.png', '14.png',
-                   '15.png', '16.png',
-                   '17.png', '18.png',
-                   '19.png', '20.png',
-                   '21.png', '22.png',
-                   '23.png', '24.png'];
+let backgroundList = ['01.png', '02.png', 
+                      '03.png', '04.png',
+                      '05.png', '06.png', 
+                      '07.png', '08.png', 
+                      '09.png', '10.png', 
+                      '11.png', '12.png',
+                      '13.png', '14.png',
+                      '15.png', '16.png',
+                      '17.png', '18.png',
+                      '19.png', '20.png',
+                      '21.png', '22.png',
+                      '23.png', '24.png'];
+
+let pictureList = ['tartan01.jpeg', 
+                   'tartan02.jpeg', 
+                   'tartan03.jpeg', 
+                   'tartan04.jpg',
+                   'tartan05.jpeg', 
+                   'tartan06.jpg', 
+                   'tartan07.jpg',  
+                   'tartan08.jpeg', 
+                   'tartan09.jpg', 
+                   'tartan10.jpeg', 
+                   'tartan11.jpg',
+                   'tartan12.png'];
+
 
 let timeBackGroundArray = [13, 21, 34, 55, 89, 144];
 let timeBackground;
@@ -44,6 +60,7 @@ let inter;
 
 let choiceArray = [colorFade, noColorFade];
 let choice;
+let sec, min, h, day;  
 
 
 //-----------------------------------------------PRELOAD
@@ -51,9 +68,10 @@ function preload() {
   //ruggito = new Tone.Player(baseURLSound + "ruggito.mp3").toDestination();
   logo = loadImage(baseURLImage + 'good one white.png');
   
-  back = loadImage(baseURLBack + random(pictureList));
-  
-  img = loadImage(baseURLImage + 'klee.jpg');
+  back = loadImage(baseURLBack + random(backgroundList));
+  h = hour()%12;
+
+  img = loadImage(baseUrlPictures + pictureList[h]);
   //back2 = loadImage(baseURLBack + '12.png');
 }
 //------------------------------------------------WINDOWRESIZED
@@ -69,7 +87,7 @@ function setup() {
   
   timeBackground = random(timeBackGroundArray);
   setInterval(reloadPage, 1000*timeBackground);
-  console.log(timeBackground);
+  console.log(timeBackground, h);
   
   img.resize(100, 0);
   img.loadPixels();
